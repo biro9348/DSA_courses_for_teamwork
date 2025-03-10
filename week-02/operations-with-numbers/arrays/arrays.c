e579b8b3cc18210b62092e96cca219b6c568d220
biro9348 <birobali05@gmail.com>
2025-03-10 08:17:33 +0200

    2025.03.10

--- week-02/operations-with-numbers/arrays/arrays.c
+++ week-02/operations-with-numbers/arrays/arrays.c
@@ -1,5 +1,12 @@
 //
 // Created by palma on 2/23/2025.
 //
-
+#include <time.h>
+#include <stdlib.h>
 #include "arrays.h"
+void fillWithRandom(int *array, int n, int start, int end) {
+    srand (time(0));
+    for(int i=0;i<n;i++) {
+        array[i] = start + rand()% (end -start);
+    }
+}
--- week-02/operations-with-numbers/utils/utils.c
+++ week-02/operations-with-numbers/utils/utils.c
@@ -3,3 +3,9 @@
 //
 
 #include "utils.h"
+#include <stdio.h>
+void printArray(int *array, int n) {
+    for(int i=0;i<n;i++) {
+        printf("%d ",array[i]);
+    }
+}
